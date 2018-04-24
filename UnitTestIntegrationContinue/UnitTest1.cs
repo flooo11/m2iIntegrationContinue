f3d48ca4b1b6d6a497fe70d0d422fa4db146063c
Marc BALAND <flooo11@hotmail.com>
2018-04-24 11:17:43 +0200

    Ajout du premier test unitaire

---
+++ UnitTestIntegrationContinue/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using IntegrationContinue;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestIntegrationContinue
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void Test_GetInt()
+        {
+            Assert.AreEqual(Calcul.GetInt("3"), 3);
+        }
+    }
+}
