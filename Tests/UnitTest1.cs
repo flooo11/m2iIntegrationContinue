5669617cb245f3c7c9209d26a72fab77cb1d59f1
Marc BALAND <flooo11@hotmail.com>
2018-04-24 11:38:23 +0200

    Intégration des tests du formateur et déchargement de mes tests suite à des problème liés à NuGet

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using IntegrationContinue;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsNotNull(Calcul.GetInt("20"));
+            Assert.AreEqual(Calcul.GetInt("40"), 40);
+            Assert.AreEqual(Calcul.GetInt("50"), 50);
+            Assert.AreEqual(Calcul.GetInt("1000"), 1000);
+        }
+    }
+}
